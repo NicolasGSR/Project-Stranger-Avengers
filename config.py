7a733a0fbf893e3d0d196e9185e548fdcf0b0a5a
Nicolas Reis <nicolasgosore@gmail.com>
2023-06-01 08:40:22 -0300

    novo commit

--- config.py
+++ config.py
@@ -1,10 +1,8 @@
-"""Configurações necessarias"""
-
 from marvel import Marvel
 from keys import PUBLIC_KEY, PRIVATE_KEY
 
 marvel = Marvel(PUBLIC_KEY=PUBLIC_KEY, PRIVATE_KEY=PRIVATE_KEY)
 
-SECRET_KEY = '*********'
+SECRET_KEY = '***********'
 
-SQLALCHEMY_DATABASE_URI = "*************"
+SQLALCHEMY_DATABASE_URI = "************************************"
